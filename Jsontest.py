3cc6090c3d7c374ce911821b155b5534cb4c8456
juan brinez <juan.brinez@loras.edu>
2021-06-29 18:53:08 +0000

    initial commit

---
+++ Jsontest.py
@@ -0,0 +1,22 @@
+import requests
+import spotipy
+
+CLIENT_ID = '3880d25476064e1ab4e91538b1f0d566'
+CLIENT_SECRET = 'db42db2aabf742a7908141b84a9727bc'
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+#AUTH_URL = 'https://developer.spotify.com/dashboard/applications/3880d25476064e1ab4e91538b1f0d566' #URL of the page
+
+auth_response = requests.post(AUTH_URL, {
+                            'grant_type': 'client_credentials',
+                            'client_id' : CLIENT_ID,
+                            'client_secret' : CLIENT_SECRET,
+})
+
+auth_response_data = auth_response.json()
+print(auth_response_data)
+#print(auth_response.status_code)
+
+
+
+
+
