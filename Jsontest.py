354ed46eafc82b06420f2572bbdd3eba5f0775c1
juan brinez <juan.brinez@loras.edu>
2021-06-30 16:51:42 +0000

    first update

--- Jsontest.py
+++ Jsontest.py
@@ -3,8 +3,7 @@ import spotipy
 
 CLIENT_ID = '3880d25476064e1ab4e91538b1f0d566'
 CLIENT_SECRET = 'db42db2aabf742a7908141b84a9727bc'
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-#AUTH_URL = 'https://developer.spotify.com/dashboard/applications/3880d25476064e1ab4e91538b1f0d566' #URL of the page
+AUTH_URL = 'https://accounts.spotify.com/api/token' #URL of the page
 
 auth_response = requests.post(AUTH_URL, {
                             'grant_type': 'client_credentials',
@@ -14,7 +13,18 @@ auth_response = requests.post(AUTH_URL, {
 
 auth_response_data = auth_response.json()
 print(auth_response_data)
-#print(auth_response.status_code)
+
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+BASE_URL = 'https://api.spotify.com/v1/'
+artist_id = '36QJpDe2go2KgaRleHCDTp'
+
+r = requests.get(BASE_URL + 'artists/' + artist_id + '/albums', headers=headers, params={'include_groups': 'album', 'limit' : 50})
+d = r.json()
+print(d)
 
 
 
