06bd583556bd839ff4ecd89829b345dee7c3c3e1
juan brinez <juan.brinez@loras.edu>
2021-06-30 23:59:43 +0000

    3 style update

--- Jsontest.py
+++ Jsontest.py
@@ -14,7 +14,7 @@ AUTH_URL = 'https://accounts.spotify.com/api/token'  # URL of the page
 auth_response = requests.post(AUTH_URL, {
                             'grant_type': 'client_credentials',
                             'client_id': CLIENT_ID,
-                            'client_secret' : CLIENT_SECRET,
+                            'client_secret': CLIENT_SECRET,
 })
 
 auth_response_data = auth_response.json()
@@ -26,15 +26,15 @@ headers = {
 }
 
 BASE_URL = 'https://api.spotify.com/v1/'
-#BASE_URL = 'https://open.spotify.com/'
+# BASE_URL = 'https://open.spotify.com/'
 artist_id = '36QJpDe2go2KgaRleHCDTp'
-#artist_id = '2CIMQHirSU0MQqyYHq0eOx?si=FMm2w-AgQ3OIWbq0WOmo2A&dl_branch=1'
+# artist_id = '2CIMQHirSU0MQqyYHq0eOx?si=FMm2w-AgQ3OIWbq0WOmo2A&dl_branch=1'
 
-r = requests.get(BASE_URL + 'artists/' + artist_id + 
+r = requests.get(BASE_URL + 'artists/' + artist_id +
                  '/albums/', headers=headers, 
                  params={'include_groups': 'album', 'limit' : 10})
 d = r.json()
-#print(d)
+# print(d)
 
 i = 0
 for album in d['items']:
@@ -43,8 +43,7 @@ for album in d['items']:
 
 # print(Dic)
 
-pf = pd.DataFrame.from_dict(Dic, orient='index',
-                       columns=['Name', 'Release_Date'])
+pf = pd.DataFrame.from_dict(Dic, orient='index', columns=['Name', 'Release_Date'])
 
 engine = create_engine('mysql://root:codio@localhost/FirstAPI')
 
